9b26b8b51b880e55a0fbaa6c97b84f937a8aa723
igor <igor.soldatov@gmail.com>
2018-10-09 12:13:24 +0300

    init #2

--- unet_resnet/unet_resnet_v3/search_lr.py
+++ unet_resnet/unet_resnet_v3/search_lr.py
@@ -8,14 +8,7 @@ fold = 4
 
 pipeline = Pipeline(train_fold='../folds/train_fold_{}.csv'.format(fold), valid_fold='../folds/valid_fold_{}.csv'.format(fold))
 
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='dice_loss')
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='bce_dice_loss')
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='bce_logdice_loss')
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='weighted_bce_dice_loss')
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='lovasz_loss')
-pipeline.training_stage_1(epochs=1, batch_size=32, fold=fold, lr=0.001, loss='binary_crossentropy')
-
-# for lr in np.linspace(0.001, 0.0001, 10):
-#     pipeline.training_stage_1(epochs=100, batch_size=32, fold=fold, lr=lr)
+for lr in np.linspace(0.001, 0.0001, 10):
+    pipeline.training_stage_1(epochs=100, batch_size=32, fold=fold, lr=lr)
 
 
