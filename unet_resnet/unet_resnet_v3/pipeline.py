fc5a89b5c9aaa5127d7889bb88cb6360bc90e6bb
igor <igor.soldatov@gmail.com>
2018-10-09 12:30:43 +0300

    fixed

--- unet_resnet/unet_resnet_v3/pipeline.py
+++ unet_resnet/unet_resnet_v3/pipeline.py
@@ -71,19 +71,19 @@ class Pipeline:
             print(len(self.train_df))
 
             self.train_df["images"] = [
-                np.array(load_img("../../input/train/images/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/images/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.train_df.index)]
             self.train_df["masks"] = [
-                np.array(load_img("../../input/train/masks/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/masks/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.train_df.index)]
             self.valid_df["images"] = [
-                np.array(load_img("../../input/train/images/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/images/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.valid_df.index)]
             self.valid_df["masks"] = [
-                np.array(load_img("../../input/train/masks/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/masks/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.valid_df.index)]
 
@@ -101,7 +101,7 @@ class Pipeline:
             #     print(train_index.shape, evaluate_index.shape)  # the shape is slightly different in different cv, it's OK
 
             self.x_test = np.array(
-                [(np.array(load_img("../../input/test/images/{}.png".format(idx), color_mode="grayscale"))) / 255 for
+                [(np.array(load_img("../../input/test/images/{}.png".format(idx), grayscale=True))) / 255 for
                  idx in
                  tqdm_notebook(self.test_df.index)]).reshape(-1, self.img_size_target, self.img_size_target, 1)
         else:
@@ -113,11 +113,11 @@ class Pipeline:
             print(len(self.train_df))
 
             self.train_df["images"] = [
-                np.array(load_img("../../input/train/images/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/images/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.train_df.index)]
             self.train_df["masks"] = [
-                np.array(load_img("../../input/train/masks/{}.png".format(idx), color_mode="grayscale")) / 255 for idx
+                np.array(load_img("../../input/train/masks/{}.png".format(idx), grayscale=True)) / 255 for idx
                 in
                 tqdm_notebook(self.train_df.index)]
 
@@ -133,10 +133,7 @@ class Pipeline:
                 print(train_index.shape,
                       evaluate_index.shape)  # the shape is slightly different in different cv, it's OK
 
-            self.x_test = np.array(
-                [(np.array(load_img("../../input/test/images/{}.png".format(idx), color_mode="grayscale"))) / 255 for
-                 idx in
-                 tqdm_notebook(self.test_df.index)]).reshape(-1, self.img_size_target, self.img_size_target, 1)
+            self.x_test = np.array( [ ( np.array(load_img("../../input/test/images/{}.png".format(idx), grayscale=True) ) / 255 for idx in tqdm_notebook(self.test_df.index) ] ).reshape(-1, self.img_size_target, self.img_size_target, 1)
 
     def training_stage_1(self, epochs, batch_size=32, fold=0, lr=0.001, loss='binary_crossentropy'):
         # training
