4746eb25fc6f602be7a38b6d0bbb2b0f267fe84b
igor <igor.soldatov@gmail.com>
2018-10-10 23:20:37 +0300

    update #3

--- unet_resnet/unet_resnet_v3/search_lr.py
+++ unet_resnet/unet_resnet_v3/search_lr.py
@@ -2,13 +2,14 @@ from pipeline import *
 from losses import *
 
 fold = 4
-epochs = 2
+epochs = 200
 batch_size = 64
 # lr = 0.001
 
 pipeline = Pipeline(train_fold='../folds/train_fold_{}.csv'.format(fold), valid_fold='../folds/valid_fold_{}.csv'.format(fold))
 
 for lr in np.linspace(0.0009, 0.0001, 9):
-    pipeline.training_stage_2(epochs=epochs, batch_size=batch_size, fold=fold, stage=2, lr=lr, loss='dice_loss', weights='../stage_01/fold_04/weights/weights_stage_01_loss_dice_loss.model')
+    pipeline.training_stage_2(epochs=epochs, batch_size=batch_size, fold=fold, stage=2, lr=lr, loss='dice_loss',
+                              custom_objects={'my_iou_metric': my_iou_metric, 'dice_loss': dice_loss}, weights='../stage_01/fold_04/weights/weights_stage_01_loss_dice_loss.model')
 
 
